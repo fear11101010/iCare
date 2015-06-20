d06ae44177ab481f79e8a730ca0b7d0ddcf05bca
fear11101010 <fear11101010@users.noreply.github.com>
2015-06-20 13:26:08 +0600

    validate height and weight
    lol

--- iCare/app/src/main/java/com/example/arafathossain/icare/ProfileValidation.java
+++ iCare/app/src/main/java/com/example/arafathossain/icare/ProfileValidation.java
@@ -10,6 +10,8 @@ public class ProfileValidation {
     private static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                    private static final String HEIGHT_PATTERN = "^[0-9]{1,3}(\\.[0-9]+)?\\s?(ft|m|cm)$";
+    private static final String WEIGHT_PATTERN = "^[0-9]{1,3}(\\.[0-9]+)?\\s?(kg|lb|pound|gm)$";
 
     public static boolean validateEmail(String email) {
         if (email == null || email.isEmpty()) return false;
@@ -31,5 +33,17 @@ public class ProfileValidation {
     public static boolean validateDateOfBirth(String dob) {
         if (dob == null || dob.isEmpty()) return false;
         else return true;
+    }
+    public static boolean validateHeight(String height) {
+        if (height == null || height.isEmpty()) return false;
+        Pattern heightPattern = Pattern.compile(HEIGHT_PATTERN);
+        Matcher matcher = heightPattern.matcher(height);
+        return matcher.matches();
+    }
+    public static boolean validateWeight(String weight) {
+        if (weight == null || weight.isEmpty()) return false;
+        Pattern weightPattern = Pattern.compile(WEIGHT_PATTERN);
+        Matcher matcher = weightPattern.matcher(weight);
+        return matcher.matches();
     }
 }
